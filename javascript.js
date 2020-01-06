ef92e9cc11d363a6d164ee82219559299c41831e
Cantonissen <54833786+Cantonissen@users.noreply.github.com>
2020-01-06 12:07:59 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 10; i++) {
+    console.log("i heeft nu de waarde van " + i);
+}
+
+var mycar = {
+    brand: "Mazda",
+    type: "MX-5",
+    wheels: "4",
+    color: "silver",
+}
+
+console.log(mycar.color);

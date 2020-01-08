abb799430891ea51bd6701df4f909a2b9a920a8c
Cantonissen <54833786+Cantonissen@users.noreply.github.com>
2020-01-08 10:17:58 +0100

    second commit

---
+++ H02/Opdracht2/javascript.js
@@ -0,0 +1,18 @@
+const bl1 =  document.getElementById("bl1");
+bl1.style.backgroundColor = "cyan";
+bl1.style.textAlign = "center";
+bl1.style.paddingTop = "20px";
+
+const bl2 = document.getElementById("bl2");
+bl2.style.backgroundColor = "green";
+bl2.style.textAlign = "center";
+bl2.style.marginTop = "20px";
+
+const bl3 = document.getElementById("bl3");
+bl3.style.textAlign = "right";
+bl3.style.marginTop = "40px";
+
+const bl4 = document.getElementById("bl4")
+bl4.style.textAlign = "left";
+bl4.style.backgroundColor = "red";
+
